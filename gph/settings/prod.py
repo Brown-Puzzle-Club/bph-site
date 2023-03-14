6fa47b4c09787792236458c16f0fb7c319588ebc
obloomfield <obloomfield@brown.edu>
2023-03-14 00:57:25 -0400

    add GA

--- gph/settings/prod.py
+++ gph/settings/prod.py
@@ -6,15 +6,20 @@ IS_TEST = False
 
 # Used for constructing URLs; include the protocol and trailing
 # slash (e.g. 'https://galacticpuzzlehunt.com/')
-DOMAIN = 'https://brownpuzzlehunt.com/'
+DOMAIN = 'https://www.brownpuzzlehunt.com/'
 
 # List of places you're serving from, e.g.
 # ['galacticpuzzlehunt.com', 'gph.example.com']; or just ['*']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.brownpuzzlehunt.com']
 
 # Google Analytics
 GA_CODE = '''
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-24DY5C1PC8"></script>
 <script>
-  /* FIXME */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-24DY5C1PC8');
 </script>
 '''
