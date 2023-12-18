aae0cb4ecada7daa6f6c67a1e32e59f9870b2d6a
Nicholas Bottone <nick@bottone.io>
2023-12-17 21:09:53 -0500

    Add some mess

--- react-demo/src/routes/Root.tsx
+++ react-demo/src/routes/Root.tsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
+import { usePageContext } from "../hooks/usePageContext";
 
+import react2Logo from "../assets/react2.svg";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../assets/vite.svg";
+import vite2Logo from "../assets/vite2.svg";
+import thing from "../assets/thing.svg";
 
-import { useState } from 'react';
-import { usePageContext } from '../hooks/usePageContext';
+react2Logo;
 
 export default function Root() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const pageContext = usePageContext<{test: number, test2: number}>();
-  const test = pageContext?.test
-  const test2 = pageContext?.test2
+  const pageContext = usePageContext<{ test: number; test2: number }>();
+  const test = pageContext?.test;
+  const test2 = pageContext?.test2;
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} alt="Vite logo" />
+      </a>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={vite2Logo} alt="Vite2 logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} alt="React logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={react2Logo} alt="React2 logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={thing} className="thing" alt="thing" />
+      </a>
+      <h1>Vite + React (+ Django eventually)! 🤪🤪🤪</h1>
       <div className="card">
         <p>test: {test}</p>
         <p>test2: {test2}</p>
@@ -22,5 +43,5 @@ export default function Root() {
       </div>
       <a href="/route-two">routeTwo</a>
     </>
-  )
+  );
 }
--- react-demo/vite.config.ts
+++ react-demo/vite.config.ts
@@ -1,22 +1,23 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',  // Adjust the URL to match your Django server
+      "/api": "http://localhost:8000", // Adjust the URL to match your Django server
     },
   },
   build: {
-    outDir: `../puzzles/static/react`,/*`../puzzles/react-build`,*/
+    outDir: `../puzzles/static/react` /*`../puzzles/react-build`,*/,
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
-    }
-  }
-})
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+});
