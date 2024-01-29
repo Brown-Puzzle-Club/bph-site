f3f6dcef1a747bfc531873d931ba27995695a86d
Nicholas Bottone <nick@bottone.io>
2024-01-29 14:40:58 -0500

    Add rest viewsets

---
+++ puzzles/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import permissions, viewsets, mixins
+from .serializers import *
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)  # type: ignore
+
+
+class TeamViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    serializer_class = TeamSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Team.objects.filter(user=self.request.user)
+
+
+class BasicTeamViewSet(mixins.RetrieveModelMixin,
+                       mixins.ListModelMixin,
+                       viewsets.GenericViewSet):
+    serializer_class = TeamBasicSerializer
