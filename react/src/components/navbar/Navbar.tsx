df15750e42966fe4d7238dbbcb7a5bd06cc971eb
Aren Guralp <142130640+guralpa@users.noreply.github.com>
2024-02-09 01:10:28 -0500

    Fix navbar button padding.

--- react/src/components/navbar/Navbar.tsx
+++ react/src/components/navbar/Navbar.tsx
@@ -138,7 +138,7 @@ const NavbarLeft = () => {
             <NavigationMenuItem>
               <HuntLogo/>
             </NavigationMenuItem>
-            <NavigationMenuItem>
+            <NavigationMenuItem className="pl-6">
               <NavigationMenuTrigger>The Hunt</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
