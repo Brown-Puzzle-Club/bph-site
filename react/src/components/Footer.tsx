65fc24d22269323c66cc68dbff8f18641d465738
obloomfield <obloomfield@brown.edu>
2024-02-18 13:21:06 -0500

    add instagram link

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -1,9 +1,11 @@
 
+import { FaInstagram } from "react-icons/fa"
+
 export default function Footer() {
   return (
-    // text centered, slate
     <div className="footer bg-intherit text-white text-center p-3">
-      <span className="text-muted-foreground">powered by <a className="underline font-semibold" href="https://github.com/galacticpuzzlehunt/gph-site/tree/master">gph-site</a></span>
+      <a href="https://www.instagram.com/brownpuzzlehunt/" className="border-r-2 border-muted-foreground pr-3"><FaInstagram className="inline-block text-muted-foreground hover:text-white transition-colors" size={24} /></a>
+      <span className="text-muted-foreground pl-3">powered by <a className="underline font-semibold hover:text-white transition-colors" href="https://github.com/galacticpuzzlehunt/gph-site/tree/master">gph-site</a></span>
     </div>
   )
 }
