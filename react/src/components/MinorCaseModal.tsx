6b007b8695d6f48d733bec95ee724883ead0d99e
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2024-03-09 17:22:46 -0500

    added minor case stuff

--- react/src/components/MinorCaseModal.tsx
+++ react/src/components/MinorCaseModal.tsx
@@ -1,5 +1,5 @@
 // Modal.tsx
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Round } from "@/utils/django_types";
 import manila from "@/assets/main/manila_open.png";
@@ -21,6 +21,10 @@ const MinorCaseModal: React.FC<ModalProps> = ({
   onSubmit,
   cur_case,
 }) => {
+  useEffect(() => {
+    console.log(cur_case?.id);
+  }, [cur_case]);
+
   console.log(isOpen);
   if (!isOpen) {
     return null;
@@ -43,30 +47,32 @@ const MinorCaseModal: React.FC<ModalProps> = ({
     <>
       <div className="fixed inset-0 flex items-center justify-center text-[black]">
         <div
-          className="flex h-1/2 w-3/5 flex-row rounded-md bg-white p-6"
+          className="flex h-auto w-3/5 flex-row rounded-md p-6 pl-8"
           style={{
             backgroundImage: `url(${manila})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            width: "50rem",
           }}
         >
-          <div className="w-2/4">{cur_case_art}</div>
+          <div className="w-7/12">{cur_case_art}</div>
           <div></div>
           <div></div>
-          <div className="grid w-2/4 grid-rows-9">
+          <div className="grid w-5/12 grid-cols-1 grid-rows-9">
             {/*Div containg all puzzle info */}
-            <div className="row-span-1 mb-4 grid grid-cols-4">
+            <div className="row-span-1 mb-4 grid w-full grid-cols-4">
               <h2 className="col-span-3 text-xl text-purple-500">{cur_case?.name}</h2>
               <button className="col-span-1 flex justify-end self-end" onClick={closeModal}>
                 X
               </button>
             </div>
-            <h3 className="row-span-7">{cur_case?.description}</h3>
-
+            <h3 className="row-span-7">{cur_case?.description} description text</h3>{" "}
+            {/* className="row-span-7" */}
             {/* Link to the minor case page */}
-            <Link className="col-span-3 text-xl text-purple-500" to={`/minorcase/${cur_case?.id}`}>
+            <Link className="text-xl text-purple-500" to={`/minorcase/${cur_case?.id}`}>
               Go to Minor Case Page
             </Link>
-
-            <div className="flex justify-center">
+            <div className="flex justify-between">
               {/* Container for centering */}
               <div>{cur_case?.name}</div>
               <button className="row-span-1 self-end" onClick={submitVote}>
@@ -78,6 +84,42 @@ const MinorCaseModal: React.FC<ModalProps> = ({
           </div>
         </div>
       </div>
+      {/* <div className="fixed inset-0 flex items-center justify-center">
+          <div
+            className="bg-white p-6 rounded-md w-3/5 h-1/2 flex-row flex"
+            style={
+              {
+                // backgroundImage: manila,
+              }
+            }
+          >
+            <div className="w-2/4">Art assets</div>
+            <div></div>
+            <div></div>
+            <div className="w-2/4 grid grid-rows-9">
+              {/*Div containg all puzzle info 
+              <div className="grid grid-cols-4 mb-4 row-span-1">
+                <h2 className="col-span-3 text-purple-500 text-xl">
+                  {cur_case?.name}
+                </h2>
+                <button
+                  className="col-span-1 self-end flex justify-end"
+                  onClick={closeModal}
+                >
+                  X
+                </button>
+              </div>
+              <h3 className="row-span-7">{cur_case?.description}</h3>
+              <div className="flex justify-center">
+                {/* Container for centering 
+                <button className="self-end row-span-1" onClick={submitVote}>
+                  Enter
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div> */}
     </>
   );
 };
--- react/src/utils/main/constants.tsx
+++ react/src/utils/main/constants.tsx
@@ -1,9 +1,9 @@
 import { TestCaseArt } from "@/components/minor_cases/minor_case_art";
 
 interface CaseArtByRoundSlug {
-  [key: string]: JSX.Element;
+  [key: number]: JSX.Element;
 }
 
 export const CASE_ART_BY_ROUND_SLUG: CaseArtByRoundSlug = {
-  test_case: <TestCaseArt />,
+  2: <TestCaseArt />,
 };
