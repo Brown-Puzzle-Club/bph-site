89ab2475c4ce57259baeb00306d3dca6bf61563f
obloomfield <obloomfield@brown.edu>
2024-03-09 15:14:36 -0500

    cleanup vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
-if (process.env.NODE_ENV == "production") {
-  console.log("~ RUNNING IN DJANGO PROD MODE ~");
-}
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -15,23 +11,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8000", // Adjust the URL to match your Django server
+      "/api": "http://localhost:8000",
     },
   },
-
-  build: {
-    outDir: `../puzzles/static/react`,
-
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
-  base: process.env.NODE_ENV == "production" ? "/static/react/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
