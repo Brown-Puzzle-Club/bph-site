7ad4e08f59dab7cd532145f7ac7c976e8c665657
obloomfield <obloomfield@brown.edu>
2024-03-13 02:54:27 -0400

    markdown testing

---
+++ client/src/routes/MarkdownTest.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import "../styles/puzzlestyle.css";
+
+export default function MarkdownTest() {
+  const [markdown, setMarkdown] = useState("");
+
+  return (
+    <div className="text-white">
+      {/* MARKDOWN INPUT BOX */}
+      <div className="flex justify-center">
+        <div className="w-1/2">
+          <textarea
+            className="w-full h-96 p-4 bg-gray-900 text-white"
+            placeholder="Markdown goes here..."
+            onChange={(e) => setMarkdown(e.target.value)}
+          ></textarea>
+        </div>
+      </div>
+      {/* HTML RESULT BOX */}
+      <div className="flex justify-center pt-6">
+        <div className="w-1/2">
+          <h1 className="text-3xl text-center pb-5">Markdown Result</h1>
+          <Markdown
+            className="bg-muted-100 p-6 border border-gray-500 border-dashed rounded puzzle-styling"
+            remarkPlugins={[remarkGfm]}
+          >
+            {markdown}
+          </Markdown>
+        </div>
+      </div>
+    </div>
+  );
+}
