79f384df14cc02f18f909931ea3118923e3ddc83
obloomfield <obloomfield@brown.edu>
2024-04-04 12:09:32 -0400

    add colored thread assets

--- client/src/routes/major_cases/ColoredThread.tsx
+++ client/src/routes/major_cases/ColoredThread.tsx
@@ -1,6 +1,20 @@
+import background from "@/assets/images/major_cases/colored-thread/background.jpg";
+import board from "@/assets/images/major_cases/colored-thread/board.png";
 import SVGBoard from "@/components/major_cases/colored-thread/SVGBoard";
 
 export default function ColoredThread() {
   // return <Board />;
-  return <SVGBoard />;
+  return (
+    <div>
+      <div
+        className={"map relative left-1/2 transform -translate-x-1/2 max-w-screen-xl w-full h-full"}
+      >
+         <img className="art-bg-img" src={board} />
+        <div className="art-bg">
+          <img className="art-bg-img" src={background} />
+        </div>
+      </div>
+      <SVGBoard />
+    </div>
+  );
 }
