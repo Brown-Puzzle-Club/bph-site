e46cb22850db275c95c2ecbff9af0852e2bdc71c
obloomfield <obloomfield@brown.edu>
2024-04-08 05:40:14 -0400

    add a simple framer motion

--- client/src/components/main_page/CompletedCases.tsx
+++ client/src/components/main_page/CompletedCases.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import type { MinorCaseCompleted, Round } from "@/utils/django_types";
 
 import Cases from "./Cases";
@@ -14,7 +16,12 @@ export default function CompletedCases({
   return (
     completed_cases &&
     solvedCasesOpen && (
-      <div className="flex flex-col items-center">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0 }}
+        className="flex flex-col items-center"
+      >
         <span className="font-bold text-[2vw] text-center mb-4 bg-slate-800 rounded-xl">
           COMPLETED CASES
         </span>
@@ -22,7 +29,7 @@ export default function CompletedCases({
         <div className="flex flex-wrap overflow-y-auto max-h-[20vw] p-4 space-x-3 space-y-3 rounded-xl bg-[#09090947]">
           <Cases casesRecord={completed_cases} setSelectedCase={setSelectedCase} />
         </div>
-      </div>
+      </motion.div>
     )
   );
 }
