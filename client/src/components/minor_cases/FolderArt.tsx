23ef2c50e0f1f1be6bd96e7c9d9c1fedeeaf4833
obloomfield <obloomfield@brown.edu>
2024-04-12 16:07:44 -0400

    more folder arts :)))

--- client/src/components/minor_cases/FolderArt.tsx
+++ client/src/components/minor_cases/FolderArt.tsx
@@ -1,20 +1,33 @@
+import datasticker1 from "@/assets/main_page/folders/dt_trim1.png";
+import datasticker2 from "@/assets/main_page/folders/dt_trim2.png";
+import rtpaper1 from "@/assets/main_page/folders/rt_trim1.png";
 import blood from "@/assets/main_page/folders/sd_blood.png";
 import sd_trim2 from "@/assets/main_page/folders/sd_trim2.png";
 import tape from "@/assets/main_page/folders/tape.png";
 import birb from "@/assets/minor_cases/birbs/teaser-1.png";
 import birbyarn from "@/assets/minor_cases/birbs/teaser-2.png";
 import birbcoin from "@/assets/minor_cases/birbs/teaser-3.png";
+import catmeme from "@/assets/minor_cases/cats/catmeme.png";
 import clip1 from "@/assets/minor_cases/clipping1.png";
 import clip2 from "@/assets/minor_cases/clipping2.png";
 import exile1 from "@/assets/minor_cases/exile/exile_teaser1.png";
 import exile2 from "@/assets/minor_cases/exile/exile_teaser2.png";
+import tax2 from "@/assets/minor_cases/financial_crimes/1040.png";
+import tax1 from "@/assets/minor_cases/financial_crimes/w9.png";
 import labyrinth from "@/assets/minor_cases/labyrinth/labyrinth2.png";
+import lockbox from "@/assets/minor_cases/maze/lockbox.png";
 import micro1 from "@/assets/minor_cases/microinfluencer/micro-teaser2.png";
 import nyt1 from "@/assets/minor_cases/nyt/nyt-teaser1.png";
 import nyt2 from "@/assets/minor_cases/nyt/nyt-teaser2.png";
 import nyt3 from "@/assets/minor_cases/nyt/nyt-teaser3.png";
+import pennytitle from "@/assets/minor_cases/penny/pennytitle.png";
+import wreath from "@/assets/minor_cases/penny/wreath.png";
 import twiqh2 from "@/assets/minor_cases/twiqh/twiqh.png";
+import anchor from "@/assets/minor_cases/whales/anchor.png";
+import whales1 from "@/assets/minor_cases/whales/whales-teaser1.png";
+import whales2 from "@/assets/minor_cases/whales/whales-teaser2.png";
 
+// COLORED THREAD
 export const BirbsCaseArt = () => {
   return (
     <div>
@@ -42,6 +55,97 @@ export const BirbsCaseArt = () => {
         className="absolute aspect-square object-cover w-[15vw]"
         style={{ left: "20%", top: "12%", rotate: "-15deg" }}
         src={birbyarn}
+      />
+    </div>
+  );
+};
+
+export const WhalesArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute shadow-lg aspect-square object-cover w-[8vw]"
+        style={{ left: "10%", top: "16%", rotate: "345deg" }}
+        src={whales2}
+      />
+      <img
+        className="absolute shadow-lg aspect-square object-cover w-[10vw]"
+        style={{ left: "8%", top: "55%", rotate: "15deg" }}
+        src={clip1}
+      />
+      <img
+        className="absolute shadow-lg aspect-square object-cover w-[8vw]"
+        style={{ left: "28%", top: "30%", rotate: "5deg" }}
+        src={clip2}
+      />
+      <img
+        className="absolute shadow-lg aspect-square object-cover w-[15vw]"
+        style={{ left: "22%", top: "44%", rotate: "23deg" }}
+        src={whales1}
+      />
+      <img
+        className="absolute object-cover w-[15vw]"
+        style={{ left: "20%", top: "-12%", rotate: "3deg" }}
+        src={anchor}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "20%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+    </div>
+  );
+};
+
+export const PennyArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute shadow-lg  object-cover w-[29vw]"
+        style={{ left: "5%", top: "26%", rotate: "354deg" }}
+        src={pennytitle}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "20%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "6%", top: "12%", rotate: "295deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[27vw]"
+        style={{ left: "6%", top: "47%", rotate: "0deg" }}
+        src={rtpaper1}
+      />
+      <img
+        className="absolute object-cover w-[27vw]"
+        style={{ left: "6%", top: "48%", rotate: "0deg" }}
+        src={wreath}
+      />
+    </div>
+  );
+};
+
+export const CatsArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute shadow-lg  object-cover w-[29vw]"
+        style={{ left: "5%", top: "26%", rotate: "354deg" }}
+        src={catmeme}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "20%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "6%", top: "12%", rotate: "295deg" }}
+        src={tape}
       />
     </div>
   );
@@ -135,40 +239,19 @@ export const NYTCaseArt = () => {
         src={nyt2}
       />
       <img
+        className="absolute object-cover w-[10vw]"
+        style={{ left: "17%", top: "37%", rotate: "348deg" }}
+        src={tape}
+      />
+      <img
         className="absolute object-cover w-[29vw]"
         style={{ left: "3%", top: "53%", rotate: "0deg" }}
         src={nyt3}
       />
-      {/* <img
-        className="absolute object-cover w-[29vw]"
-        style={{ left: "%", top: "55%", rotate: "0deg", zIndex: -1 }}
-        src={sdtrim1}
-      /> */}
       <img
         className="absolute object-cover w-[20vw]"
         style={{ left: "8%", top: "60%", rotate: "15deg", opacity: "90%" }}
         src={blood}
-      />
-    </div>
-  );
-};
-export const MicroinfluencerCaseArt = () => {
-  return (
-    <div>
-      <img
-        className="absolute object-cover w-[18vw]"
-        style={{ left: "12%", top: "5%", rotate: "0deg" }}
-        src={micro1}
-      />
-      <img
-        className="absolute aspect-square w-[35vw]"
-        style={{ left: "2%", top: "10%", rotate: "0deg", zIndex: -1 }}
-        src={blood}
-      />
-      <img
-        className="absolute object-cover w-[18vw]"
-        style={{ left: "20%", top: "2%", rotate: "10deg" }}
-        src={tape}
       />
     </div>
   );
@@ -217,6 +300,93 @@ export const LabyrinthCaseArt = () => {
   );
 };
 
+// DATA
+
+export const MicroinfluencerCaseArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "12%", top: "5%", rotate: "0deg" }}
+        src={micro1}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "20%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[14vw]"
+        style={{ left: "25%", top: "73%", rotate: "354deg" }}
+        src={datasticker1}
+      />
+    </div>
+  );
+};
+export const LockboxArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute shadow-lg object-cover w-[22vw]"
+        style={{ left: "8%", top: "10%", rotate: "0deg" }}
+        src={lockbox}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "26%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "-1%", top: "23%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[14vw]"
+        style={{ left: "12%", top: "73%", rotate: "354deg" }}
+        src={datasticker1}
+      />
+      <img
+        className="absolute object-cover w-[14vw]"
+        style={{ left: "25%", top: "36%", rotate: "354deg" }}
+        src={datasticker2}
+      />
+    </div>
+  );
+};
+export const FinancialCrimesArt = () => {
+  return (
+    <div>
+      <img
+        className="absolute object-cover w-[22vw]"
+        style={{ left: "12%", top: "57%", rotate: "3deg" }}
+        src={tax2}
+      />
+      <img
+        className="absolute shadow-lg object-cover w-[22vw]"
+        style={{ left: "8%", top: "10%", rotate: "0deg" }}
+        src={tax1}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "26%", top: "2%", rotate: "10deg" }}
+        src={tape}
+      />
+      <img
+        className="absolute object-cover w-[18vw]"
+        style={{ left: "-5%", top: "61%", rotate: "42deg" }}
+        src={tape}
+      />
+
+      <img
+        className="absolute object-cover w-[15vw]"
+        style={{ left: "27%", top: "44%", rotate: "354deg" }}
+        src={datasticker1}
+      />
+    </div>
+  );
+};
+
 export const CASE_ART_BY_ROUND_SLUG: Record<string, JSX.Element> = {
   "birbs-at-brown": <BirbsCaseArt />,
   exile: <ExileCaseArt />,
@@ -224,4 +394,9 @@ export const CASE_ART_BY_ROUND_SLUG: Record<string, JSX.Element> = {
   microinfluencer: <MicroinfluencerCaseArt />,
   twiqh: <TWIQHCaseArt />,
   "god-of-the-labyrinth": <LabyrinthCaseArt />,
+  whales: <WhalesArt />,
+  penny: <PennyArt />,
+  cats: <CatsArt />,
+  maze: <LockboxArt />,
+  "financial-crimes": <FinancialCrimesArt />,
 };
--- client/src/routes/minor_cases/lockbox/LockboxIframe.tsx
+++ client/src/routes/minor_cases/lockbox/LockboxIframe.tsx
@@ -1,8 +1,14 @@
 export default function LockboxIframe() {
   return (
     <div className="flex flex-col items-center">
-      <iframe src="https://itch.io/embed/2580916" width="552" height="167" frameBorder="0">
-        <a href="https://xenonhawk.itch.io/lockbox">LOCKBOX by Xenonhawk</a>
+      <iframe
+        frameBorder="0"
+        src="https://itch.io/embed-upload/10147742?color=333333"
+        allowFullScreen
+        width="750"
+        height="770"
+      >
+        <a href="https://xenonhawk.itch.io/lockbox">Play LOCKBOX on itch.io</a>
       </iframe>
     </div>
   );
