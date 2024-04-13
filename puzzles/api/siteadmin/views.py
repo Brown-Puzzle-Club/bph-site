198a44f42dc25cd7358edffd6ecd06a407182bbc
obloomfield <obloomfield@brown.edu>
2024-04-13 18:56:09 -0400

    update solve count admin api part two

--- puzzles/api/siteadmin/views.py
+++ puzzles/api/siteadmin/views.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from puzzles.api.api_guards import require_admin
-from puzzles.models import AnswerSubmission, MinorCaseCompleted, TeamMember, Team
+from puzzles.models import (
+    AnswerSubmission,
+    EventCompletion,
+    MajorCaseCompleted,
+    MinorCaseCompleted,
+    TeamMember,
+    Team,
+)
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
@@ -54,15 +61,62 @@ def get_remote_emails(request: Request) -> Response:
 def get_team_case_solve_count(request: Request) -> Response:
 
     visible_teams = Team.objects.filter(is_hidden=False)
-    team_case_solve_count = {}
-    solves = MinorCaseCompleted.objects.all()
-    for solve in solves:
+
+    team_major_case_solve_count = {}
+    major_case_solves = MajorCaseCompleted.objects.all()
+    for solve in major_case_solves:
         team = solve.team
         if team not in visible_teams:
             continue
-        if team.team_name in team_case_solve_count:
-            team_case_solve_count[team.team_name][0] += 1
+        if team.team_name in team_major_case_solve_count:
+            team_major_case_solve_count[team.team_name] += 1
         else:
-            team_case_solve_count[team.team_name] = [1, "🐻" if team.in_person else "🖥️"]
+            team_major_case_solve_count[team.team_name] = 1
 
-    return Response({"data": team_case_solve_count})
+    team_minor_case_solve_count = {}
+    minor_case_solves = MinorCaseCompleted.objects.all()
+    for solve in minor_case_solves:
+        team = solve.team
+        if team not in visible_teams:
+            continue
+        if team.team_name in team_minor_case_solve_count:
+            team_minor_case_solve_count[team.team_name] += 1
+        else:
+            team_minor_case_solve_count[team.team_name] = 1
+
+    team_puzzle_solve_count = {}
+    puzzle_solves = AnswerSubmission.objects.filter(is_correct=True)
+    for solve in puzzle_solves:
+        team = solve.team
+        if team not in visible_teams:
+            continue
+        if team.team_name in team_puzzle_solve_count:
+            team_puzzle_solve_count[team.team_name] += 1
+        else:
+            team_puzzle_solve_count[team.team_name] = 1
+
+    team_event_solve_count = {}
+    event_solves = EventCompletion.objects.all()
+    for solve in event_solves:
+        team = solve.team
+        if team not in visible_teams:
+            continue
+        if team.team_name in team_event_solve_count:
+            team_event_solve_count[team.team_name] += 1
+        else:
+            team_event_solve_count[team.team_name] = 1
+
+    end_data = []
+    for team in visible_teams:
+        end_data.append(
+            {
+                "team_name": team.team_name,
+                "in_person": team.in_person,
+                "major_case_solves": team_major_case_solve_count.get(team.team_name, 0),
+                "minor_case_solves": team_minor_case_solve_count.get(team.team_name, 0),
+                "puzzle_solves": team_puzzle_solve_count.get(team.team_name, 0),
+                "event_solves": team_event_solve_count.get(team.team_name, 0),
+            }
+        )
+
+    return Response({"data": end_data})
