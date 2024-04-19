9f85ae679f7626309a894c6cf83df8f962719ca4
obloomfield <obloomfield@brown.edu>
2024-04-19 17:03:53 -0400

    fixed wordle bug

--- puzzles/api/puzzlehandlers/wordle_api.py
+++ puzzles/api/puzzlehandlers/wordle_api.py
@@ -4,30 +4,42 @@ from rest_framework.request import Request
 
 from ..api_actions import handle_answer
 
+
 @api_view(["POST"])
 def verify_guess(request: Request) -> Response:
+    django_context = request._request.context
+    request_context = request.context
+    if not django_context.team or "wordle" not in django_context.team.unlocks:
+        return Response({"error": "Puzzle is not unlocked"}, status=400)
+
     guess = request.data["guess"].lower()
-    if len(guess) != 5 :
+    if len(guess) != 5:
         return Response({"error": "invalid guess"}, status=400)
 
     answer = "NOOSE".lower()
 
     answer_letters = list(answer)
-    verification_array = []
+    verification_array = [
+        "incorrect",
+        "incorrect",
+        "incorrect",
+        "incorrect",
+        "incorrect",
+    ]
 
-    for guess_char, answer_char in zip(guess, answer):
+    # check for correct guesses
+    for i, (guess_char, answer_char) in enumerate(zip(guess, answer)):
         if guess_char == answer_char:
-            verification_array.append("correct")
+            verification_array[i] = "correct"
             answer_letters.remove(guess_char)
-        elif guess_char in answer_letters:
-            verification_array.append("miss")
+
+    for i, (guess_char, answer_char) in enumerate(zip(guess, answer)):
+        # check for misses
+        if guess_char in answer_letters:
+            verification_array[i] = "miss"
             answer_letters.remove(guess_char)
-        else:
-            verification_array.append("incorrect")
 
     if all(map(lambda x: x == "correct", verification_array)):
-        django_context = request._request.context
-        request_context = request.context
         # answer is a query parameter:
         handle_answer(guess, request_context, django_context, "wordle")
 
